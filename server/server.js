832e6067ae8696fd60a0f4f52ef15847c4293fbd
Sourabh Nihore <souravnihore1998@gmail.com>
2018-12-25 21:52:14 +0530

    recreate chat app

--- server/server.js
+++ server/server.js
@@ -1,38 +1,44 @@
-const path = require('path');
-const http = require('http');
-
 const express = require('express');
 const socketIO = require('socket.io');
 
+const http = require('http');
+const path = require('path');
 const pathPublic = path.join(__dirname, '/../', 'public');
 const port = process.env.PORT || 3000;
+// console.log(pathPublic);
+
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 var app = express();
+
 var server = http.createServer(app);
 
 app.use(express.static(pathPublic));
 
-var io = socketIO(server);
+io = socketIO(server);
 
 io.on('connection', (socket)=>{
-    console.log('New Connection is established.');
+    console.log('Server is connected to client ...');
 
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to Node Chat App'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'));
 
     socket.on('disconnect', ()=>{
-        console.log('New Connection disconnected.');
+        console.log('Client is disconnect from a server ...');
     });
 
-   socket.on('createMessage', (message)=>{
-    // console.log(message);
-    socket.broadcast.emit('readMessage', {
-        from: message.from,
-        text: message.text
+    socket.on('createMessage', (msg)=>{
+        // console.log('MESSAGE:\n', msg);
+        io.emit('newMessage', generateMessage(msg.from, msg.text));
     });
-   });
-    
+
+    socket.on('createLocationMessage', (msg)=>{
+        io.emit('newLocationMessage', generateLocationMessage(msg.from, msg.lat, msg.lon));
+    });
+
 });
 
 server.listen(port, ()=>{
-    console.log(`Server is listen on port ${port}`);
-    //https://immense-temple-31043.herokuapp.com/
+    console.log(`Server listen on Port ${port} ...`);
 });
